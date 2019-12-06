899b8448bcb5cdf8402d8d71e62b23f3a860999b
huxiao154 <cdhuxiao9@jd.com>
2019-12-06 16:15:46 +0800

    t3

--- src/main/java/com/hx/codeRepository/App.java
+++ src/main/java/com/hx/codeRepository/App.java
@@ -6,12 +6,11 @@ import java.util.Date;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        Date date=new Date();
-        Date date1=new Date();
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		Date date = new Date();
+		Date date1 = new Date();
+		Date date2 = new Date();
+	}
 }
